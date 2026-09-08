fcf5caa23378f2bc9f0e18949699c3a006044252
Juan Fernando Mona Cano <juanf.mona@udea.edu.co>
2026-09-07 21:10:29 -0500

    Hexagonal Architecture - Initial structure

---
+++ src/main/java/com/frabric/credits_core/CreditsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.frabric.credits_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditsCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditsCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/frabric/credits_core/CreditsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frabric.credits_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditsCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
