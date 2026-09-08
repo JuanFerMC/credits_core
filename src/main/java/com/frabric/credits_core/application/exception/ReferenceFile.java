48e8ae989bc9259620334aa41f68bd47bb668399
Juan Fernando Mona Cano <juanf.mona@udea.edu.co>
2026-09-07 21:27:28 -0500

    More reference files

---
+++ src/main/java/com/frabric/credits_core/application/exception/ReferenceFile.java
@@ -0,0 +1,5 @@
+package com.frabric.credits_core.application.exception;
+
+public class ReferenceFile {
+
+}
---
+++ src/main/java/com/frabric/credits_core/infrastructure/adapter/in/web/exception/ReferenceFile.java
@@ -0,0 +1,5 @@
+package com.frabric.credits_core.infrastructure.adapter.in.web.exception;
+
+public class ReferenceFile {
+
+}
